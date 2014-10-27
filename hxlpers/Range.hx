8bc15f33521953496fe43d705ca038fffac11d75
damrem <damien.remars@gmail.com>
2014-10-27 19:27:51 +0100

    repackaging

---
+++ hxlpers/Range.hx
@@ -0,0 +1,34 @@
+package hxlpers;
+
+class Range
+{
+	var _min:Float;
+	public var min(get_min, null):Float;
+	var _max:Float;
+	public var max(get_max, null):Float;
+	
+	public function new(Min:Float, Max:Float)
+	{
+		_min = Min;
+		_max = Max;
+		
+		if (_min > _max)
+		{
+			var temp = _min;
+			_min = _max;
+			_max = temp;
+		}
+	}
+	
+	function get_min():Float 
+	{
+		return _min;
+	}
+	
+	function get_max():Float 
+	{
+		return _max;
+	}
+	
+}
+
---
+++ hxlpers/geom/V2d.hx
@@ -0,0 +1,13 @@
+package hxlpers.geom;
+import flash.geom.Point;
+
+/**
+ * ...
+ * @author damrem
+ */
+class V2d extends Point
+{
+
+	
+	
+}
